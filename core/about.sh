d4d99abcae1c2f92a4287b004a3ab89b74ee2f20
TTX <69615429+Tetrodotoxin2-0@users.noreply.github.com>
2020-08-16 11:48:31 +0600

    Create about.sh

---
+++ core/about.sh
@@ -0,0 +1,53 @@
+clear
+
+echo '                    
+
+ ____o__ __o____  ____o__ __o____  \o       o/ 
+
+  /   \   /   \    /   \   /   \    v\     /v  
+
+       \o/              \o/          <\   />   
+
+        |                |             \o/     
+
+       < >              < >             |      
+
+        |                |             / \     
+
+        o                o           o/   \o   
+
+       <|               <|          /v     v\  
+
+       / \              / \        />       <\ 
+
+                                                 
+
+' | lolcat
+
+echo " "
+
+echo "                             About"|lolcat
+
+echo " "
+
+echo "       🙏 Hey, there I am Tetrodotoxin 2.0(TTX), i made this tool
+
+  to penetrate password strength In termux, so i hope guys you
+
+                             liked it. 😘"
+
+echo ""
+
+echo "                  GitHub  :- /Tetrodotoxin2-0"| lolcat
+
+echo "         Facebook Page  :- 𝚃𝚎𝚊𝚖-𝚃𝚎𝚝𝚛𝚘𝚍𝚘𝚝𝚘𝚡𝚒𝚗"| lolcat
+
+echo "                Instagram :- @Tetrodotoxin2.0"| lolcat
+
+echo " "
+
+sleep 5.0
+
+cd $HOME/IG-Attack
+
+bash ig-attack.sh
